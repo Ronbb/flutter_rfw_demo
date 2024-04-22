1c25d80a71e05fb206893d3f7751c41547359656
ronbb <ronbiaobiao@gmail.com>
2024-04-22 16:10:37 +0800

    chore: Update pubspec.yaml with re_editor and re_highlight dependencies and add RobotoMono font family

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:re_editor/re_editor.dart';
+import 'package:re_highlight/languages/dart.dart';
+import 'package:re_highlight/styles/github.dart';
 import 'package:rfw/formats.dart';
 import 'package:rfw/rfw.dart';
 
 const _defaultScript = """
-import core.widgets;
-import core.material;
+import widgets;
+import material;
 
 widget text = Text(
   text: 'Some text here', 
@@ -35,8 +38,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.indigo,
+      ),
+      home: const Scaffold(
         body: Demo(),
       ),
     );
@@ -51,80 +58,97 @@ class Demo extends StatefulWidget {
 }
 
 class DemoState extends State<Demo> {
+  final _selectWidgetKey = GlobalKey();
   final _runtime = Runtime();
   final _data = DynamicContent();
-  var _script = _defaultScript;
+  final _codeController = CodeLineEditingController();
   var _showBackground = false;
+  var _error = "";
+  var _main = 'main';
 
-  static const _name = LibraryName(["demo"]);
+  static const _library = LibraryName(["demo"]);
 
-  void _update() {
-    _runtime.update(_name, parseLibraryFile(_script));
+  void _update({bool rebuild = true}) {
+    try {
+      _runtime.update(_library, parseLibraryFile(_codeController.text));
+      _error = "";
+    } catch (e) {
+      _error = '$e';
+    }
+    if (rebuild) {
+      setState(() {});
+    }
   }
 
   @override
   void initState() {
     super.initState();
+    _codeController.text = _defaultScript;
     _runtime.update(
-      const LibraryName(['core', 'widgets']),
+      const LibraryName(['widgets']),
       createCoreWidgets(),
     );
     _runtime.update(
-      const LibraryName(['core', 'material']),
+      const LibraryName(['material']),
       createMaterialWidgets(),
     );
-    _update();
+    _update(rebuild: false);
   }
 
   void _showSettingDialog() {
     showDialog(
       context: context,
       builder: (context) {
-        return StatefulBuilder(builder: (context, rebuild) {
-          return AlertDialog(
-            title: const Text('Settings'),
-            content: ConstrainedBox(
-              constraints: const BoxConstraints(
-                minWidth: 360.0,
+        return StatefulBuilder(
+          builder: (context, rebuild) {
+            return AlertDialog(
+              title: const Text('Settings'),
+              content: ConstrainedBox(
+                constraints: const BoxConstraints(
+                  minWidth: 360.0,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SwitchListTile(
+                      title: const Text('Show background'),
+                      value: _showBackground,
+                      onChanged: (value) {
+                        setState(() {
+                          _showBackground = value;
+                          rebuild(() {});
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SwitchListTile(
-                    title: const Text('Show background'),
-                    value: _showBackground,
-                    onChanged: (value) {
-                      setState(() {
-                        _showBackground = value;
-                        rebuild(() {});
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Close'),
-              ),
-            ],
-          );
-        });
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Close'),
+                ),
+              ],
+            );
+          },
+        );
       },
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
+            flex: 2,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
@@ -132,38 +156,133 @@ class DemoState extends State<Demo> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Wrap(
-                    spacing: 12.0,
+                    spacing: 4.0,
                     children: [
                       FilledButton.icon(
                         icon: const Icon(Icons.upgrade),
                         label: const Text('Update'),
                         onPressed: _update,
                       ),
-                      OutlinedButton.icon(
+                      TextButton.icon(
                         icon: const Icon(Icons.settings),
                         label: const Text('Settings'),
                         onPressed: _showSettingDialog,
                       ),
+                      TextButton.icon(
+                        key: _selectWidgetKey,
+                        icon: const Icon(Icons.dashboard),
+                        onPressed: () async {
+                          // dropdown menu
+                          final library = _runtime.libraries[_library];
+                          final widgets = <String>[];
+                          if (library is RemoteWidgetLibrary) {
+                            for (var widget in library.widgets) {
+                              widgets.add(widget.name);
+                            }
+                          }
+
+                          final box = _selectWidgetKey.currentContext
+                              ?.findRenderObject() as RenderBox?;
+                          final position =
+                              box?.localToGlobal(Offset.zero) ?? Offset.zero;
+                          final size = box?.size ?? Size.zero;
+
+                          final value = await showMenu<String>(
+                            context: context,
+                            position: RelativeRect.fromLTRB(
+                              position.dx,
+                              position.dy + size.height,
+                              double.infinity,
+                              double.infinity,
+                            ),
+                            initialValue: _main,
+                            items: widgets
+                                .map((w) => PopupMenuItem<String>(
+                                      value: w,
+                                      child: Text(w),
+                                    ))
+                                .toList(),
+                          );
+
+                          if (value != null) {
+                            setState(() {
+                              _main = value;
+                            });
+                          }
+                        },
+                        label: Text('Widget: $_main'),
+                      ),
+                      if (_error.isNotEmpty)
+                        TextButton.icon(
+                          style: TextButton.styleFrom(
+                            foregroundColor: colors.error,
+                          ),
+                          icon: const Icon(Icons.error_outline),
+                          label: const Text('Errors'),
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text('Errors'),
+                                  content: Text(_error),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text('Close'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                        ),
                     ],
                   ),
                   const SizedBox(
                     height: 16,
                   ),
                   Expanded(
-                    child: TextFormField(
-                      initialValue: _script,
-                      expands: true,
-                      maxLines: null,
-                      textAlignVertical: TextAlignVertical.top,
-                      decoration: InputDecoration(
-                        constraints: const BoxConstraints.expand(),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16.0),
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 2,
+                          color: colors.primary,
                         ),
+                        borderRadius: BorderRadius.circular(16.0),
                       ),
-                      onChanged: (value) {
-                        _script = value.replaceAll('\r', '');
-                      },
+                      child: CodeEditor(
+                        controller: _codeController,
+                        wordWrap: true,
+                        autofocus: true,
+                        style: CodeEditorStyle(
+                          fontSize: 14,
+                          fontFamily: 'RobotoMono',
+                          codeTheme: CodeHighlightTheme(
+                            languages: {
+                              'rfwtxt': CodeHighlightThemeMode(
+                                mode: langDart,
+                              ),
+                            },
+                            theme: githubTheme,
+                          ),
+                        ),
+                        indicatorBuilder: (context, editingController,
+                            chunkController, notifier) {
+                          return Row(
+                            children: [
+                              DefaultCodeLineNumber(
+                                controller: editingController,
+                                notifier: notifier,
+                              ),
+                              const SizedBox(width: 8),
+                            ],
+                          );
+                        },
+                      ),
                     ),
                   ),
                 ],
@@ -171,18 +290,18 @@ class DemoState extends State<Demo> {
             ),
           ),
           Expanded(
-            flex: 2,
+            flex: 3,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: AnimatedContainer(
                 duration: const Duration(milliseconds: 300),
                 color: _showBackground
-                    ? Theme.of(context).colorScheme.secondaryContainer
-                    : Theme.of(context).colorScheme.surface,
+                    ? colors.secondaryContainer
+                    : colors.surface,
                 child: RemoteWidget(
                   runtime: _runtime,
                   data: _data,
-                  widget: const FullyQualifiedWidgetName(_name, 'main'),
+                  widget: FullyQualifiedWidgetName(_library, _main),
                   onEvent: (String name, DynamicMap arguments) {
                     debugPrint(
                         'user triggered event "$name" with data: $arguments');
